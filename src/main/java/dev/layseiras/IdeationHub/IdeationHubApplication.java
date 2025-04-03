c6caac43a3e7873fac3a032161bff687fe261383
Laysa <laysa.developer@gmail.com>
2025-04-03 12:15:24 -0300

    first commit

---
+++ src/main/java/dev/layseiras/IdeationHub/IdeationHubApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.IdeationHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeationHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeationHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/IdeationHub/IdeationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.IdeationHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
